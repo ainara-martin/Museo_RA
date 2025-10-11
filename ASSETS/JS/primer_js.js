abf81fa894a4ad4ffa5c7ca1f37857f649c297ff
ainara-martin <ainaramartinalcalde@gmail.com>
2025-10-11 13:12:25 +0200

    cambio_js

--- ASSETS/JS/primer_js.js
+++ ASSETS/JS/primer_js.js
@@ -1,14 +1,14 @@
 $(function(){
 
-    $(".seccion").hide();
     $("#indice").show();
+    $(".seccion").not("#indice").hide();
 
     $(".nav-btn").on("click", function(event) {
         event.preventDefault();
 
         const seccion = $(this).data("seccion");
 
-        $(".seccion").hide();
+        $(".seccion").not("#indice").hide();
 
         $("#" + seccion).show();
 
