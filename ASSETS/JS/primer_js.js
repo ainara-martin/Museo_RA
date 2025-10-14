fa6adce6eca6116880d252b638a0d483449de6fa
ainara-martin <ainaramartinalcalde@gmail.com>
2025-10-14 18:23:31 +0200

    jquery

--- ASSETS/JS/primer_js.js
+++ ASSETS/JS/primer_js.js
@@ -24,23 +24,23 @@ $(function(){
         formulario.on("submit", function(event) {
             event.preventDefault();
 
-            // Buscar dentro del formulario los inputs
+            
             const nombre = formulario.find("input").eq(0).val().trim();
             const apellidos = formulario.find("input").eq(1).val().trim();
             const email = formulario.find("input[type='email']").val().trim();
             const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
-            // Validación básica
+            
             if (nombre === "" || apellidos === "" || !emailRegex.test(email)) {
                 alert("Por favor, completa todos los campos correctamente.");
                 return;
             }
 
-            // Mostrar modal de confirmación
+            
             const modal = new bootstrap.Modal($("#modalConfirmacion"));
             modal.show();
 
-            // Limpiar formulario
+            
             formulario[0].reset();
         });
     });
@@ -52,13 +52,13 @@ $(function(){
 
         const buscador = $(this).closest("form").find("input[type='search']").val().toLowerCase();
 
-        // Muestra la sección de productos
+        
         $("div.seccion").hide();
         $("#productos").show();
         $("a.nav-btn").removeClass("active");
         $("a.nav-btn[data-seccion='productos']").addClass("active");
 
-        // Filtrar los proyectos (cards)
+        
         $("#productos .card").each(function() {
             const texto = $(this).text().toLowerCase();
             if (texto.includes(buscador)) {
